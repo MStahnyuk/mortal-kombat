94491d504a99368b34ecb7505e0ec971d10671a3
MStahnyuk <maks.stahnyuk@gmail.com>
2023-07-05 15:46:04 +0300

    added html stages to ChoiceScreen

--- src/Components/ChoiceScreen/index.js
+++ src/Components/ChoiceScreen/index.js
@@ -6,6 +6,7 @@ import ImageReptileStance from './images/characters/Reptile_stance.gif';
 import ImageReptileAvatar from './images/characters/Reptile_avatar.png';
 import ImageSonyaStance from './images/characters/Sonya_stance.gif';
 import ImageSonyaAvatar from './images/characters/Sonya_avatar.png';
+import {useState} from "react";
 
 const characters = [
     {
@@ -101,17 +102,27 @@ const characters = [
 ];
 
 function ChoiceScreen() {
+    const [activeCharacterFirst, setActiveCharacterFirst] = useState(characters[0]);
+    const [activeCharacterSecond, setActiveCharacterSecond] = useState(characters[1]);
 
     return <div className={styles.root}>
         <h1 className={styles.title}>Select your fighter</h1>
         <div className={styles.characters}>
             {characters.map((character, index) => <div className={classNames(
                 styles.character,
-                { [styles.activeFirst]: index === 0 },
-                { [styles.activeSecond]: index === 4 }
+                { [styles.activeFirst]: character.id === activeCharacterFirst.id },
+                { [styles.activeSecond]: character.id === activeCharacterSecond.id }
             )}>
                 <img className={styles.characterAvatar} src={character.avatar} alt=""/>
             </div>)}
+            <div className={styles.stances}>
+                <div className={styles.stance}>
+                    <img className={styles.stanceImage} src={activeCharacterFirst.image} alt=""/>
+                </div>
+                <div className={classNames(styles.stance, styles.stanceSecond)}>
+                    <img className={styles.stanceImage} src={activeCharacterSecond.image} alt=""/>
+                </div>
+            </div>
         </div>
         <h2 className={styles.location}>Combat zone: Soul chamber</h2>
     </div>
